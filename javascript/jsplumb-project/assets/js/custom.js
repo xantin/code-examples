e801047179bf2a19c594a7b0f5fde075de7bcfdf
David Dutkovsky <david.dutkovsky@gmail.com>
2016-12-02 05:04:49 +0800

    Add jsplumb demo (not finished)

---
+++ javascript/jsplumb-project/assets/js/custom.js
@@ -0,0 +1,20 @@
+// Draggable
+
+// Animation
+// var tl = new TimelineLite({onComplete:function() {
+//     this.restart()}
+//     });
+
+//     tl.addLabel("start");
+
+//     tl.to("#alsterdorf-chevron", 1, {opacity:1, y:10}, "-=0")
+//     tl.to("#alsterdorf-chevron", 1, {opacity:0}, "-=0");
+
+// var tl2 = new TimelineLite({onComplete:function() {
+//     this.restart()}
+//     });
+
+//     tl2.addLabel("start");
+
+//     tl2.to("#norderstedt-chevron", 1, {opacity:1, y:-10}, "-=0")
+//     tl2.to("#norderstedt-chevron", 1, {opacity:0}, "-=0");
