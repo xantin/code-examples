ed11c1258b5cc5961ead118b0e934cbeb377c1b0
David Dutkovsky <david.dutkovsky@gmail.com>
2016-12-07 15:44:35 +0800

    Node: update google api - generate test suspicious files

--- nodejs/google_drive_api/generateTestHashes.js
+++ nodejs/google_drive_api/generateTestHashes.js
@@ -1,5 +1,30 @@
+'use strict';
 const Chance = require("chance");
-var chance = new Chance();
-let name=chance.string({length: 40, pool: 'abcdefghijklmopqrstuvwxyz0123456789'});
-
-
+const fs = require("fs");
+const async = require("async");
+const chance = new Chance();
+let opts={length: 40, pool: 'abcdefghijklmopqrstuvwxyz0123456789'};
+let folderName='hashes';
+const createFiles=400000;
+let count = 1;
+async.whilst(
+    function() { return count <= createFiles; },
+    function(callback) {
+        let name=chance.string(opts);
+        let filePath=`${folderName}/${name}`;
+        fs.stat(filePath, (err)=>{
+            if(err){
+                return fs.writeFile(filePath, '', (err) => {
+                    if (err){
+                        return callback(err);
+                    }
+                    callback(null, count++);
+                });
+            }
+            callback(null, count)
+        });
+    },
+    function (err, n) {
+        console.log('finished');
+    }
+);
